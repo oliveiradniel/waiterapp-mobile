5566955a0fb3f22475b1dcd90e6e7bfe2884a15b
oliveiradniel <kadadniel@gmail.com>
2024-12-23 12:22:07 -0300

    style: add 'GeneralSans' font to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+import { useFonts } from 'expo-font';
+
 import { SafeAreaView, Text } from 'react-native';
 
 export default function App() {
+  const [isFontsLoaded] = useFonts({
+    'GeneralSans-400': require('./assets/fonts/GeneralSans-Regular.otf'),
+    'GeneralSans-500': require('./assets/fonts/GeneralSans-Semibold.otf'),
+    'GeneralSans-600': require('./assets/fonts/GeneralSans-Bold.otf'),
+  });
+
+  if (!isFontsLoaded) {
+    return null;
+  }
+
   return (
     <SafeAreaView>
-      <Text>Hello JStack</Text>
+      <Text style={{ fontFamily: 'GeneralSans-400' }}>Hello JStack</Text>
     </SafeAreaView>
   );
 }
