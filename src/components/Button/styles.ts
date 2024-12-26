869145de7a8a3afb1ca90cce5d0517c879a2f626
oliveiradniel <kadadniel@gmail.com>
2024-12-26 13:47:57 -0300

    feat(Button): add styles when 'Button' component is disabled

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components/native';
 
 export const Container = styled.TouchableOpacity`
   align-items: center;
-  background-color: #d73035;
+  background-color: ${({ disabled }: { disabled: boolean }) => disabled ? '#999' : '#d73035'};
   border-radius: 48px;
   justify-content: center;
   padding: 14px 24px;
