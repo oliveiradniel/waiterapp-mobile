7755cfb1c05e99219401399de37f59512cd3866d
oliveiradniel <kadadniel@gmail.com>
2025-01-01 22:24:11 -0300

    feat(Cart): add sumary to 'Cart' component

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import { FlatList, TouchableOpacity } from 'react-native';
 
 import { formatCurrency } from '../../utils/formatCurrency';
@@ -6,9 +8,10 @@ import { CartItem } from '../../types/CartItem';
 
 import { Text } from '../Text';
 
-import { Actions, Image, Item, ProductContainer, ProductDetails, QuantityContainer } from './styles';
+import { Actions, Image, Item, ProductContainer, ProductDetails, QuantityContainer, Sumary, TotalContainer } from './styles';
 import { PlusCircle } from '../Icons/PlusCircle';
 import { MinusCircle } from '../Icons/MinusCircle';
+import { Button } from '../Button';
 
 interface CartProps {
   cartItems: CartItem[];
@@ -16,42 +19,54 @@ interface CartProps {
 
 export function Cart({ cartItems }: CartProps) {
   return (
-    <FlatList
-      data={cartItems}
-      keyExtractor={cartItem => cartItem.product._id}
-      showsVerticalScrollIndicator={false}
-      renderItem={({ item: cartItem }) => (
-        <Item>
-          <ProductContainer>
-            <Image
-              source={{
-                uri: `http://192.168.18.92:3001/uploads/${cartItem.product.imagePath}`
-              }}
-            />
+    <>
+      <FlatList
+        data={cartItems}
+        keyExtractor={cartItem => cartItem.product._id}
+        showsVerticalScrollIndicator={false}
+        style={{ marginBottom: 20, maxHeight: 150 }}
+        renderItem={({ item: cartItem }) => (
+          <Item>
+            <ProductContainer>
+              <Image
+                source={{
+                  uri: `http://192.168.18.92:3001/uploads/${cartItem.product.imagePath}`
+                }}
+              />
 
-            <QuantityContainer>
-              <Text color='#666' size={14}>{cartItem.quantity}</Text>
-            </QuantityContainer>
+              <QuantityContainer>
+                <Text color='#666' size={14}>{cartItem.quantity}</Text>
+              </QuantityContainer>
 
-            <ProductDetails>
-              <Text size={14} weight={600}>{cartItem.product.name}</Text>
-              <Text color='#666' size={14} style={{
-                margiTop: 4
-              }}>{formatCurrency(cartItem.product.price)}</Text>
-            </ProductDetails>
-          </ProductContainer>
+              <ProductDetails>
+                <Text size={14} weight={600}>{cartItem.product.name}</Text>
+                <Text color='#666' size={14} style={{
+                  margiTop: 4
+                }}>{formatCurrency(cartItem.product.price)}</Text>
+              </ProductDetails>
+            </ProductContainer>
 
-          <Actions>
-            <TouchableOpacity>
-              <PlusCircle />
-            </TouchableOpacity>
+            <Actions>
+              <TouchableOpacity>
+                <PlusCircle />
+              </TouchableOpacity>
 
-            <TouchableOpacity>
-              <MinusCircle />
-            </TouchableOpacity>
-          </Actions>
-        </Item>
-      )}
-    />
+              <TouchableOpacity>
+                <MinusCircle />
+              </TouchableOpacity>
+            </Actions>
+          </Item>
+        )}
+      />
+
+      <Sumary>
+        <TotalContainer>
+          <Text color='#666'>Total</Text>
+          <Text size={20} weight={600}>{formatCurrency(120)}</Text>
+        </TotalContainer>
+
+        <Button onPress={() => alert('confirmar pedido')}>Confirmar pedido</Button>
+      </Sumary>
+    </>
   );
 }
--- src/components/Cart/styles.ts
+++ src/components/Cart/styles.ts
@@ -14,7 +14,7 @@ export const ProductContainer = styled.View`
 export const Image = styled.Image`
   border-radius: 6px;
   height: 40px;
-  width: 40px;
+  width: 60px;
 `;
 
 export const QuantityContainer = styled.View`
@@ -22,11 +22,17 @@ export const QuantityContainer = styled.View`
   min-width: 20px;
 `;
 
-export const ProductDetails = styled.View`
-
-`;
+export const ProductDetails = styled.View``;
 
 export const Actions = styled.View`
   flex-direction: row;
   gap: 24px;
 `;
+
+export const Sumary = styled.View`
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const TotalContainer = styled.View``;
